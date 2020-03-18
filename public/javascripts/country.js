18bcaaef9ca845aad5f1e8c2142745bf70c28875
JazzHdezZ <jazmin.zepeda@smiledirectclub.com>
2020-03-18 08:09:45 -0600

    first commit

---
+++ public/javascripts/country.js
@@ -0,0 +1,12 @@
+var selectedOption = '';
+var optionsCountry= [
+      { value: '', text: 'Please select a country' },
+      { value: 'US', text: 'United States' },
+      { value: 'AU', text: 'Australia' },
+      { value: 'CA', text: 'Canada' },
+      { value: 'DE', text: 'Germany' },
+      { value: 'HK', text: 'Hong Kong' },
+      { value: 'IE', text: 'Ireland' },
+      { value: 'NZ', text: 'New Zealand' },
+      { value: 'GB', text: 'United Kingdom' },
+    ];
